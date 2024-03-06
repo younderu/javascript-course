426ab5a172b02a01d1e9a224f9544b4bbdf1c51e
helloq <91028892+younderu@users.noreply.github.com>
2024-03-06 07:14:29 +0200

    Update script.js

--- LAB2/script.js
+++ LAB2/script.js
@@ -1,11 +1,58 @@
-var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
-for () {
-
+(function() {
+  var names = [
+    "Bill",
+    "John",
+    "Jen",
+    "Jason",
+    "Paul",
+    "Frank",
+    "Steven",
+    "Larry",
+    "Paula",
+    "Laura",
+    "Jim",
+  ];
   
-  if () {
-    
-  } else {
-    
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].charAt(0).toUpperCase() === "J") {
+      SpeakGoodBye.speak(names[i])
+      //console.log("Goodbye " + names[i])
+    } else {
+    SpeakHello.speak(names[i])
+    //console.log("Hello " + names[i])
+  
+    }
   }
-}
+
+  console.log("//// New way of selection ////")
+
+  for (let i = 0; i < names.length; i++) {
+    if (sumASCII(names[i]) >= 430) {
+      SpeakGoodBye.speak(names[i])
+      //console.log("Goodbye " + names[i])
+    } else {
+    SpeakHello.speak(names[i])
+    //console.log("Hello " + names[i])
+  
+    }
+  }
+
+
+  function sumASCII(str){
+    let sum = 0
+    for(let i = 0; i < str.length; i++){
+      sum += str.charCodeAt(i)
+    }
+    return sum
+  }
+
+//console.log(sumASCII(names[4]))
+
+console.log(window)
+
+})();
+
+
+
+
