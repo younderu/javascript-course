6b9626c38b2b8dcffb893d1752dbf4de02e5033b
helloq <91028892+younderu@users.noreply.github.com>
2024-04-16 15:37:35 +0300

    Create index.js

---
+++ LAB6/index.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lights Out</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+    <div class="container" id="gridContainer"></div>
+    <div>
+      <p id="moves"></p>
+      <p id="targetMoves"></p>
+      <button id="restartButton">Restart</button>
+      <button id="newGameButton">New Game</button>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
